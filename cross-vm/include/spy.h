7e343c21472c719c98db77d4a10c91b4da238f07
Aditya Wate <aditya.wate@hotmail.com>
2016-04-25 13:37:32 -0400

    cleaned header file

--- cross-vm/include/spy.h
+++ cross-vm/include/spy.h
@@ -1,3 +1,5 @@
+//This header file has been adapted from the source of
+//http://daim.idi.ntnu.no/masteroppgaver/009/9231/vedlegg.zip
 #pragma once
 #include "exportrules.h"
 #include "asm_defines.h"
@@ -7,24 +9,6 @@
 extern "C" {
 #endif
 
-#ifdef __arm__
-static inline raw_t getCCNT(void) {
-	raw_t value;
-	// Read CCNT Register
-	asm volatile ("MRC p15, 0, %0, c9, c13, 0\t\n": "=r"(value));
-	return value;
-}
-#else 
-static inline raw_t getCCNT(void) {
-	raw_t value;  		
-    asm volatile ("rdtsc" : "=a"(value) );
-	return value;
-}
-#endif
-
-static inline raw_t delta_time(raw_t t0, raw_t t1) {
-	return t0 < t1 ? t1-t0 : t1 + (((raw_t)-1)-t0);
-}
 
 /** 
  * raw_timings must be of size: CACHE_SETS*N + 1.
