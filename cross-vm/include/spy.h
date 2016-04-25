343f45888ae1232d3f343d3963f8932071b2beb0
Aditya Wate <aditya.wate@hotmail.com>
2016-04-25 13:55:26 -0400

    removed types.h

--- cross-vm/include/spy.h
+++ cross-vm/include/spy.h
@@ -1,9 +1,9 @@
 //This header file has been adapted from the source of
 //http://daim.idi.ntnu.no/masteroppgaver/009/9231/vedlegg.zip
 #pragma once
+#include <stdint.h>
 #include "exportrules.h"
 #include "asm_defines.h"
-#include "types.h"
 
 #ifdef __cplusplus 
 extern "C" {
@@ -13,7 +13,7 @@ extern "C" {
 /** 
  * raw_timings must be of size: CACHE_SETS*N + 1.
 */
-void SSTD_API spy_loop_abs(	raw_t * raw_timings, unsigned long N);
+void SSTD_API spy_loop_abs( uint32_t * raw_timings, unsigned long N);
 
 
 #ifdef __cplusplus 
